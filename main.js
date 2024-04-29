1162be83d71d899a91a3f4e4484e230e5f76289f
Naman <namankansal91@gmail.com>
2024-04-29 05:49:48 +0530

    testing

--- main.js
+++ main.js
@@ -5,13 +5,17 @@ const app = express();
 require('dotenv').config();
 const redis = require('./redisClient');
 
-console.log("redis--->", redis);
+
 
 const PORT = process.env.PORT;
 
 
 app.use(bodyParser.json());
 app.use('/api/v1', routes);
+
+app.get('/', (req, res) => {
+    res.status(200).send(redis); 
+  });
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
